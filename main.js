d6e84afc4a9295698463a26c0846acd42f74e684
Spartan <spartan@enigma.com>
2022-04-11 21:47:20 +0200

    Count function

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+const count = birthDate => {
+    const today = new Date();
+    const year = today.getFullYear();
+    console.log((year- birthDate)*12)
+};
+
 const greet = name => console.log(name);
+
 greet('Grumpy Cat');
+count(2021);
