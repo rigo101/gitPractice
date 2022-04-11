8b39b25400a8b125a2c21108ee2e079ef32648e3
Spartan <spartan@enigma.com>
2022-04-11 22:43:07 +0200

    Future feature

--- main.js
+++ main.js
@@ -6,5 +6,10 @@ const count = birthDate => {
     console.log((year - birthDate) * 12);
 };
 
+function future(param){
+    console.log(`Your future looks like ${param}!`);
+};
+
 greet('Grumpy Kittens', 'Moon');
 count(2021);
+future('bright');
